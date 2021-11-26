aa82e7969dc85ad76211a4e8fd97174f14e5ee5e
konika <konikasingh1996@gmail.com>
2021-11-26 09:47:51 +0530

    Validate and Test the lastname

--- UserRegistrationTesting/TestProject1/UnitTest1.cs
+++ UserRegistrationTesting/TestProject1/UnitTest1.cs
@@ -6,22 +6,37 @@ namespace TestProject1
     [TestClass]
     public class UnitTest1
     {
-            [TestMethod]
-            public void ValidFirstNameOfUser()
-            {
-                //AAA method
-                //Arrange
-                string name = "Konika";
-                string expected = "invalid";
-                //creating object of moodanalyzer class and passing message
-                //RegularExpression regularExpression = new RegularExpression(name);
+         [TestMethod]
+         public void ValidFirstNameOfUser()
+         {
+               //AAA method
+              //Arrange
+              string name = "Konika";
+              string expected = "invalid";
+              //creating object of moodanalyzer class and passing message
+              //RegularExpression regularExpression = new RegularExpression(name);
 
-                //Act
-                string actual = UserRegistration.FirstNameValid(name);
+              //Act
+             string actual = UserRegistration.FirstNameValid(name);
 
-                //Assert
-                //comparing actual and expected value
-                Assert.AreEqual(expected, actual);
-            }
+              //Assert
+             //comparing actual and expected value
+             Assert.AreEqual(expected, actual);
+         }
+        [TestMethod]
+        public void ValidLastNameOfUser()
+        {
+            //AAA method
+            //Arrange
+            string lastName = "amir";
+            string expected = "valid";
+
+            //Act
+            string actual = UserRegistration.LastNameNameValid(lastName);
+
+            //Assert
+            //comparing actual and expected value
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
--- UserRegistrationTesting/UserRegistrationTesting/UserRegistration.cs
+++ UserRegistrationTesting/UserRegistrationTesting/UserRegistration.cs
@@ -29,5 +29,24 @@ namespace UserRegistrationTesting
                 return "invalid";
             }
         }
+        //This method for validate last name
+        public static string LastNameNameValid(string name)
+        {
+            //Regular expression for first letter is capital and maximum charactor is 3
+            string lastName = "^[A-Z]{1}[a-zA-Z]{2}$";
+            Regex regex = new Regex(lastName);
+            //if condition for matching pattern
+            if (regex.IsMatch(name))
+            {
+                //Console.WriteLine(name + " is valid name");
+                return "valid";
+            }
+            //else condition for if condition is false then else block will execute
+            else
+            {
+                //Console.WriteLine(name + " is invalid last name. Please start with capital letter and take maximums 3 charactors");
+                return "invalid";
+            }
+        }
     }
 }
